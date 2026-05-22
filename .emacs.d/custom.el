25dbe648c4e5e9de9d23851551e295e2266120bb
mi2ebi <merrybot47@gmail.com>
2026-05-21 20:40:16 -0700

    ligatures

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,12 +8,12 @@
  '(package-selected-packages
    '(auctex breadcrumb buffer-move cape centered-cursor-mode clang-format
             corfu diff-hl diredfl drag-stuff emmet-mode flycheck
-            flymake-easy flymake-rakudo haskell-mode helpful highlight
-            json5-ts-mode lin magit marginalia markdown-mode mips-mode
-            modus-themes moody multiple-cursors orderless org-fragtog
-            pdf-tools rainbow-delimiters raku-mode spacious-padding
-            spinner surround uiua-mode vertico vterm web-mode
-            xterm-color)))
+            flymake-easy flymake-rakudo ghostel haskell-mode helpful
+            highlight json5-ts-mode ligature lin magit marginalia
+            markdown-mode mips-mode modus-themes moody
+            multiple-cursors orderless org-fragtog pdf-tools
+            rainbow-delimiters raku-mode spacious-padding spinner
+            surround uiua-mode vertico vterm web-mode xterm-color)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
